1419c655e435e679b64ad864ec743b8a2c594f83
znewton <znewton@iastate.edu>
2017-05-13 10:08:14 -0500

    Navbar stuff

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import Navbar from './Navbar/Navbar';
+
 export default class Main extends Component {
   render() {
     return (
       <main className="Main">
+        <Navbar />
         Main
       </main>
     );
---
+++ src/Main/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+
+export default class Navbar extends Component {
+  handleSignOut() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+  render() {
+    const displayName = firebase.auth().currentUser.displayName || firebase.auth().currentUser.displayName.match(/^([^@]*)@/)[1];
+    return (
+      <nav className="Navbar">
+        <div className="nav-left">
+          <span className="logo">Social</span>
+        </div>
+        <div className="nav-middle">
+          <a href="/#/feed"><span className="fa fa-user-circle-o fa-fw" /> <span>Profile</span></a>
+          <a href="/#/feed"><span className="fa fa-bars fa-fw" /> <span>Feed</span></a>
+          <a href="/#/feed"><span className="fa fa-comments fa-fw" /> <span>Chat</span></a>
+        </div>
+        <div className="nav-right">
+          <span className="user">Hello, {displayName}</span>
+          <button onClick={this.handleSignOut.bind(this)}><span className="fa fa-unlock-alt" /> Sign Out</button>
+        </div>
+      </nav>
+    );
+  }
+}
